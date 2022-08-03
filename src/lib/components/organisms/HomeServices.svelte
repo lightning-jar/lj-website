eb81c4b1e208aa3ac36c1b40d489f4add7bb5ceb
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2022-08-03 16:53:45 -0400

    build: finish services / begin testimonials

--- src/lib/components/organisms/HomeServices.svelte
+++ src/lib/components/organisms/HomeServices.svelte
@@ -5,27 +5,50 @@
   import SectionDark from "$m/SectionDark.svelte";
   import TileServices from "$m/TileServices.svelte";
 
-  import IconWebDesign from "$a/IconWebDesign.svelte";
+  const tiles = [
+    {
+      heading: "Web Design & Development",
+      iconSlug: "icon-web-design",
+      text: "Our process-driven approach to web design and development is engineered to help your organization quickly strategize, prioritize, and execute digital experiences that matter to customers."
+    },
+    {
+      heading: "Custom Software & Apps",
+      iconSlug: "icon-custom-software",
+      text: "Non-software companies with great ideas on how to digitize their services or solutions, or solve a business challenge through custom software often need help turning their vision into reality. That\'s where we come in."
+    },
+    {
+      heading: "eCommerce, PIM, MDM",
+      iconSlug: "icon-ecommerce",
+      text: `Our technology studio specializes in PIM, DAM, MDM implementations and eCommerce transformations leveraging the <a href="/blog/what-is-pimcore" class="text-underline">power of Pimcore</a> -- the world's most flexible and powerful unified digital platform.`
+    },
+    {
+      heading: "Kiosks & In-Store Experiences",
+      iconSlug: "icon-kiosk",
+      text: "Customer experience and user experience intersect at the in-store Kiosk. Whether it's an in-room directory at a four star hotel, or a food-ordering system for a cafe, these projects demand excellence in user interface design and execution -- that's what we do."
+    },
+    {
+      heading: "Sales & Pricing Tools",
+      iconSlug: "icon-sales-tools",
+      text: "What are the challenges and friction points slowing down your sales team or agents in the call center? Too often the sales organization is the last to benefit from digital transformations -- ask yourself what happens if you turn that equation around?"
+    },
+    {
+      heading: "UX Optimization",
+      iconSlug: "icon-ux",
+      text: "There's a big gap between usability and great user experience. Our user experience team can help you push beyond interfaces that simply work to best-in-class experiences that increase affinity, encourage sales and encourage customer loyalty."
+    }
+  ]
 
 </script>
 
 <template lang='pug'>
 
-SectionDark
+SectionDark(id!="{'services'}")
   .text-center.mt-20.mb-20
-    TextHeading Digital Services
+    TextHeading(classes!="{''}") Digital Services
 
-  // services grid
+  // tiles grid
   .gap-8.grid-cols-2.mb-20(class="md:grid lg:grid-cols-3 ")
+    +each('tiles as tile')
+      TileServices("{...tile}")
 
-    // tile
-    TileServices
-      IconWebDesign(slot="icon")
-      svelte:fragment(slot="heading") Web Design &amp; Development
-      svelte:fragment(slot="text")
-        | Our process-driven approach to web design and development is engineered to help your organization quickly strategize, prioritize, and execute digital experiences that matter to customers.
-
-    div.h-24.bg-blue-500
-    div.h-24.bg-blue-500
-    div.h-24.bg-blue-500
 </template>
