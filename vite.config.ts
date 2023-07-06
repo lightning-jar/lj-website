39a779fd4404d16a0658fd0a9e0bdb79b6ad19fe
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2023-07-06 14:55:20 -0400

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import type { UserConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { sentrySvelteKit } from "@sentry/sveltekit";
+import { defineConfig } from "vitest/config";
 
-const config: UserConfig = {
-	plugins: [sveltekit()],
-};
-
-export default config;
+export default defineConfig({
+	plugins: [
+		sentrySvelteKit({
+			adapter: "vercel",
+		}),
+		sveltekit(),
+	],
+	test: {
+		include: ["src/**/*.{test,spec}.{js,ts}"],
+	},
+});
