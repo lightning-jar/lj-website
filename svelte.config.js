4889c7ed336637e8642b1d0b56482827d650a80d
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2023-07-06 14:58:11 -0400

    svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,28 +1,45 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
-import path from "path";
+import vercel from "@sveltejs/adapter-vercel";
+import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess({
-		postcss: true,
-	}),
+	preprocess: preprocess(),
 	kit: {
-		adapter: adapter(),
+		adapter: vercel({
+			// make explicit -- vercel does not yet support later node versions
+			runtime: "nodejs18.x",
+		}),
 		alias: {
-			$a: path.resolve("./src/lib/components/atoms"),
-			$m: path.resolve("./src/lib/components/molecules"),
-			$o: path.resolve("./src/lib/components/organisms"),
-      $components: path.resolve("./src/lib/components"),
-			$functions: path.resolve("./src/lib/functions"),
-			$h: path.resolve("./src/lib/functions/helpers"),
-      $settings: path.resolve("./src/lib/settings"),
-      $stores: path.resolve("./src/lib/stores"),
-      $types: path.resolve("./src/lib/types"),
-    }
-	}
+			$atoms: "./src/lib/components/atoms",
+			$assets: "./src/lib/assets",
+			$molecules: "./src/lib/components/molecules",
+			$organisms: "./src/lib/components/organisms",
+			$components: "./src/lib/components",
+			$data: "./src/lib/data",
+			$functions: "./src/lib/functions",
+			$settings: "./src/lib/settings",
+			$stores: "./src/lib/stores",
+			$types: "./src/lib/types",
+			$utils: "./src/lib/utils",
+		},
+		csp: {
+			directives: {
+				"script-src": [
+					"self",
+					"*.youtube.com",
+					"*.vimeo.com",
+					"*.jotform.com",
+					"plausible.io", // 3rd party analytics -- replaces Google Analytics
+					"*.sentry.io", // error tracking
+				],
+				"style-src": ["self", "unsafe-inline"],
+				"worker-src": ["self", "blob:"],
+				"child-src": ["self", "blob:", "*.jotform.com", "*.vimeo.com", "*.youtube.com"],
+			},
+		},
+	},
 };
 
 export default config;
