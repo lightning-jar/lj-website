0a90c861a6ebc347d97dbe090448eedc799804a5
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2023-08-22 13:53:19 -0400

    data flow for layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,23 +2,59 @@
 	// import css file
 	import "/src/app.css";
 
+	// import stores
+	import { mobileNavOpen } from "$stores/navStore";
+
+	// import analytics component
+	import PlausibleAnalytics from "$atoms/PlausibleAnalytics.svelte";
+
 	// import components
 	import GlobalNav from "$organisms/GlobalNav.svelte";
-	import GlobalFooter from "$organisms/GlobalFooter.svelte";
+
+	// local types
+	import type { LayoutData } from "./$types";
+
+	// import child page data
+	import { page } from "$app/stores";
 
 	// props
-	export let globalNavEl: HTMLHeadElement;
-	export let mainNav: HTMLDivElement;
+	export let data: LayoutData;
+
+	// variables
+	let mobileNavContainerClasses: string;
+	$: mobileNavContainerClasses = $mobileNavOpen
+		? "translate-x-0 pointer-events-auto transition-transform"
+		: "translate-x-[-100vw] pointer-events-none";
 </script>
 
 <template lang="pug">
-	.absolute.w-full
-		GlobalNav(
-			bind:globalNav!="{ globalNavEl }",
-			bind:mainNav!="{ mainNav }"
-		)
+	//- analytics
+	PlausibleAnalytics
 
+	//- head metadata
+	svelte:head
+		title { $page.data.metaTitle ?? "Sage Energy" }
+		+if('$page.data.metaDescription')
+			meta(
+				content!="{ $page.data.metaDescription }",
+				name="description"
+			)
+
+	GlobalNav(
+		isHome!="{ data.isHome }",
+		nav!="{ data.nav }"
+	)
 	slot
+	//- GlobalFooter
 
-	GlobalFooter
+	//- //- mobile nav
+	//- .fixed.top-0.left-0.z-30.h-screen.w-screen(
+	//- 	class!="{mobileNavContainerClasses} md:hidden"
+	//- )
+	//- 	//- spacer for header
+	//- 	.w-full.bg-oxford(class="h-[60px]")
+
+	//- 	//- mobile nav inner container
+	//- 	.relative.w-full.h-full.pt-8.bg-neutral-100.shadow-xl
+	//- 		//-MobileMenu(nav!="{ data.nav }")
 </template>
