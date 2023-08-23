4e5e7b9cbbbc8de7ca5a528a4726971e8a80861f
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2023-08-23 19:34:04 -0400

    formatting

---
+++ src/lib/components/atoms/GraphicDots.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let classes = "";
+</script>
+
+<template lang="pug">
+	//- use css radial gradient to create dots
+	#dot-grid(
+		class="absolute text-maximumYellow bottom-0 right-0 w-2/5 h-1/3",
+		style="background-image: radial-gradient(currentColor 20%, transparent 20%); background-position: 0 0, 50%; background-size: 10% 10%"
+	)
+</template>
