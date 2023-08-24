787e0c5860ae6074d8a05007c11ec7cb485abdef
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2023-08-24 16:43:51 -0400

    a11y: adds aria-hidden to graphic

--- src/lib/components/atoms/GraphicDots.svelte
+++ src/lib/components/atoms/GraphicDots.svelte
@@ -4,8 +4,11 @@
 
 <template lang="pug">
 	//- use css radial gradient to create dots
-	#dot-grid(
-		class="absolute text-maximumYellow bottom-0 right-0 w-2/5 h-1/3",
+	#dot-grid.pointer-events-none(
+		class!="absolute text-maximumYellow bottom-0 right-0 w-2/5 aspect-square {classes}",
+		aria-hidden="true",
 		style="background-image: radial-gradient(currentColor 20%, transparent 20%); background-position: 0 0, 50%; background-size: 10% 10%"
 	)
+		//- polyfill for aspect ratio
+		.h-0.w-full(class="pt-[100%]")
 </template>
