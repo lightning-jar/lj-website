22d03333c49d59322ea299c340e17f25ae3cf2e8
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2026-01-16 15:37:17 -0500

    Hamburger icon and updates to built with

--- src/lib/components/GlobalNav.svelte
+++ src/lib/components/GlobalNav.svelte
@@ -1,26 +1,23 @@
 <script lang="ts">
-// components
-// import ButtonHamburger from "$components/ButtonHamburger.svelte";
-import NavLogoBlock from "$components/NavLogoBlock.svelte";
+  // components
+  import HamburgerButton from "$components/HamburgerButton.svelte";
+  import NavLogoBlock from "$components/NavLogoBlock.svelte";
 
-// props
-// let { nav }: { nav: NavItem[] } = $props();
+  // refs
+  let contextMenuButton: HTMLButtonElement | null = $state(null);
 
-// refs
-let contextMenuButton: HTMLButtonElement | null = $state(null);
+  // content
+  const navItems = [
+    { label: "Home", href: "/" },
+    { label: "Services", href: "/services" },
+    { label: "Customer Stories", href: "/customer-stories" },
+    { label: "Testimonials", href: "/testimonials" },
+  ];
 
-// content
-const navItems = [
-	{ label: "Home", href: "/" },
-	{ label: "Services", href: "/services" },
-	{ label: "Customer Stories", href: "/customer-stories" },
-	{ label: "Testimonials", href: "/testimonials" },
-];
-
-// function
-function toggleContextMenu() {
-	contextMenuButton?.click();
-}
+  // function
+  function toggleContextMenu() {
+    contextMenuButton?.click();
+  }
 </script>
 
 <header
@@ -43,54 +40,29 @@ function toggleContextMenu() {
 >
   <NavLogoBlock />
 
-  <button
-    aria-label="toggle menu"
-    class="group hover:bg-slate-100/10 hover:opacity-100 h-auto grid grid-cols-1 gap-4.5px place-items-center place-content-center leading-0 relative rounded-sm py-4px px-5px opacity-60 mr-0 -top-5px"
-    popovertarget="contextMenu"
-    title="toggle menu"
-    id="context-menu-button"
-  >
-    {#each Array(3) as _item, index}
-      <div
-        class="
-          aspect-square
-          bg-slate-100
-          flex
-          h-4px
-          items-center
-          justify-center
-          leading-0
-          rounded-full
-          text-10px
-          w-4px
-          group-hover-bg-accent"
-      ></div>
-    {/each}
-  </button>
+  <div>
+    <HamburgerButton></HamburgerButton>
 
-  <div
-    popover="auto"
-    id="contextMenu"
-    class=" bg-oxfordDark relative page-x-padding w-full text-white pt-6 pb-8 min-h-screen"
-  >
-    <div class="flex relative pt-4">
-      <button
-        bind:this={contextMenuButton}
-        class="absolute top-0 right-0"
-        popovertarget="contextMenu"
-        >✕
-      </button>
-    </div>
     <div
-      class="grid grid-cols-1 gap-5 place-content-center place-items-center font-serif font-700 text-22px sm:text-30px sm:gap-6 text-accent"
+      popover="auto"
+      id="hamburger-menu"
+      class=" bg-oxfordDark absolute inset-0 page-x-padding w-full text-white pt-4 pb-8 min-h-screen lg:pt-6
+			xl:pt-8"
     >
-      {#each navItems as item}
-        <a
-          href={item.href}
-          class="opacity-90 underline-offset-4 decoration-accent/30 hover:opacity-100 hover:underline hover:decoration-accent"
-          onclick={toggleContextMenu}>{item.label}</a
-        >
-      {/each}
+      <div class="flex relative pt-0 justify-end">
+        <HamburgerButton classes="rotate-45"></HamburgerButton>
+      </div>
+      <div
+        class="grid grid-cols-1 gap-5 place-content-center place-items-center font-serif font-700 text-22px sm:text-30px lg:text-48px sm:gap-6 text-accent pt-5"
+      >
+        {#each navItems as item}
+          <a
+            href={item.href}
+            class="opacity-90 underline-offset-4 decoration-accent/30 hover:opacity-100 hover:underline hover:decoration-accent underline-offset-8 font-display"
+            onclick={toggleContextMenu}>{item.label}</a
+          >
+        {/each}
+      </div>
     </div>
   </div>
 </header>
---
+++ src/lib/components/HamburgerButton.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import { default as hamburgerIcon } from "$assets/hamburger.svg?raw";
+
+  let {
+    classes = "",
+    popovertarget = "hamburger-menu",
+    title = "toggle the hamburger menu",
+    id = "hamburger-menu-button",
+  }: {
+    classes?: string;
+    popovertarget?: string | null;
+    title?: string | null;
+    id?: string | null;
+  } = $props();
+</script>
+
+<button
+  aria-label="toggle menu"
+  class="
+  border
+  border-current
+    w-7
+    h-7
+    flex
+    group
+    hover:bg-maximumYellow-100/10
+    justify-end
+    items-center
+    max-fit-w
+    rounded-full
+    border-current
+    lg:h-9
+    lg:w-9
+    p-5px
+    overflow-hidden
+    text-slate-100
+    hover:text-accent
+    {classes}"
+  {popovertarget}
+  {title}
+  {id}
+>
+  {@html hamburgerIcon.replace("w-full h-auto", "w-full h-auto fill-current")}
+</button>
